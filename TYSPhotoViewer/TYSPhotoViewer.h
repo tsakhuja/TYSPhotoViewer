c178ee47755519ebf6d2290bdd11358f47083f30
Timothy Sakhuja <tsakhuja@gmail.com>
2013-09-27 09:58:15 -0700

    Initial Commit

---
+++ TYSPhotoViewer/TYSPhotoViewer.h
@@ -0,0 +1,13 @@
+//
+//  TYSPhotoViewer.h
+//  TYSPhotoViewer
+//
+//  Created by Timothy Sakhuja on 9/27/13.
+//  Copyright (c) 2013 Timothy Sakhuja. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TYSPhotoViewer : NSObject
+
+@end
---
+++ TYSPhotoViewerTests/TYSPhotoViewerTests.m
@@ -0,0 +1,34 @@
+//
+//  TYSPhotoViewerTests.m
+//  TYSPhotoViewerTests
+//
+//  Created by Timothy Sakhuja on 9/27/13.
+//  Copyright (c) 2013 Timothy Sakhuja. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TYSPhotoViewerTests : XCTestCase
+
+@end
+
+@implementation TYSPhotoViewerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
